2febfdc366344d077a64e42ac2efb0d450ab0b89
Paul Zietsman <phzietsman@gmail.com>
2017-12-21 21:40:35 +0200

    cose layout

--- code.js
+++ code.js
@@ -37,7 +37,7 @@ $(function () {
     dataType: 'text'
   });
 
-  var layoutOptions = {
+  var concentricLayout = {
     name: 'concentric',
 
     fit: true,
@@ -101,7 +101,95 @@ $(function () {
     spacingFactor: 2,
   };
 
+  var coseLayout = {
+    name: 'cose',
+  
+    // Called on `layoutready`
+    ready: function(){},
+  
+    // Called on `layoutstop`
+    stop: function(){},
+    
+    // Whether to animate while running the layout
+    // true : Animate continuously as the layout is running
+    // false : Just show the end result
+    // 'end' : Animate with the end result, from the initial positions to the end positions
+    animate: true,
+  
+    // Easing of the animation for animate:'end'
+    animationEasing: undefined,
+  
+    // The duration of the animation for animate:'end'
+    animationDuration: undefined,
+  
+    // A function that determines whether the node should be animated
+    // All nodes animated by default on animate enabled
+    // Non-animated nodes are positioned immediately when the layout starts
+    animateFilter: function ( node, i ){ return true; },
+  
+  
+    // The layout animates only after this many milliseconds for animate:true
+    // (prevents flashing on fast runs)
+    animationThreshold: 250,
+  
+    // Number of iterations between consecutive screen positions update
+    // (0 -> only updated on the end)
+    refresh: 20,
+  
+    // Whether to fit the network view after when done
+    fit: true,
+  
+    // Padding on fit
+    padding: 30,
+  
+    // Constrain layout bounds; { x1, y1, x2, y2 } or { x1, y1, w, h }
+    boundingBox: undefined,
+  
+    // Excludes the label when calculating node bounding boxes for the layout algorithm
+    nodeDimensionsIncludeLabels: false,
+  
+    // Randomize the initial positions of the nodes (true) or use existing positions (false)
+    randomize: false,
+  
+    // Extra spacing between components in non-compound graphs
+    componentSpacing: 40,
+  
+    // Node repulsion (non overlapping) multiplier
+    nodeRepulsion: function( node ){ return 400000; },
+  
+    // Node repulsion (overlapping) multiplier
+    nodeOverlap: 4,
+  
+    // Ideal edge (non nested) length
+    idealEdgeLength: function( edge ){ return 250; },
+  
+    // Divisor to compute edge forces
+    edgeElasticity: function( edge ){ return 100; },
+  
+    // Nesting factor (multiplier) to compute ideal edge length for nested edges
+    nestingFactor: 1.2,
+  
+    // Gravity force (constant)
+    gravity: 1,
+  
+    // Maximum number of iterations to perform
+    numIter: 1000,
+  
+    // Initial temperature (maximum node displacement)
+    initialTemp: 1000,
+  
+    // Cooling factor (how the temperature is reduced between consecutive iterations
+    coolingFactor: 0.99,
+  
+    // Lower temperature threshold (below this point the layout will end)
+    minTemp: 1.0,
+  
+    // Pass a reference to weaver to use threads for calculations
+    weaver: false
+  };
 
+  var layoutOptions = coseLayout;
+  var layoutMemory = null;
 
   // when both graph export json and style loaded, init cy
   Promise.all([cytoscapeStyles, coinmarketcap, cryptocompare, customcats, erctokens]).then(buildElements);
@@ -230,10 +318,11 @@ $(function () {
     allElements.removeClass('highlighted');
 
     if (!node.length) {
-      var resetLayout = allElements.makeLayout(layoutOptions);
 
+      var resetLayout = allElements.makeLayout(layoutOptions);
       resetLayout.run();
 
+      
       return;
     }
 
@@ -273,7 +362,7 @@ $(function () {
     var cy = window.cy = cytoscape({
       container: document.getElementById('cy'),
 
-      layout: layoutOptions,
+      // layout: layoutOptions,
 
       style: styles,
       elements: elements,
@@ -283,6 +372,14 @@ $(function () {
       minZoom: 0.1,
       maxZoom: 4,
     });
+
+    var layout = cy.layout(layoutOptions);
+    
+    layout.on('layoutstop', function( event ){
+      console.log('layoutstop promise fulfilled');
+    });
+
+    layout.run();
 
     cy.on('tap', function (evt) {
 
