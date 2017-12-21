05725a5918e03a87e370b2283b86ee1070a7f00e
Paul Zietsman <phzietsman@gmail.com>
2017-12-21 20:39:21 +0200

    Added search
    styling for search box is broken

--- code.js
+++ code.js
@@ -23,7 +23,7 @@ $(function () {
   // var erctokensUrl = "http://crossorigin.me/" + "https://eidoo.io/erc20-tokens-list/";
   var erctokensUrl = "./eidoo.io";
   // var erctokensUrl = "https://eidoo.io/erc20-tokens-list/";
-  
+
   var erctokens = $.ajax({
     crossDomain: true,
     url: erctokensUrl, // curated list of erc20 / 223 tokens
@@ -36,6 +36,71 @@ $(function () {
     type: 'GET',
     dataType: 'text'
   });
+
+  var layoutOptions = {
+    name: 'concentric',
+
+    fit: true,
+    animate: true,
+    animationDuration: 500,
+    animationEasing: 'linear',
+    avoidOverlap: true,
+    concentric: function (node) {
+      if (node.data("type") === 'consensus' || node.data("type") === 'privacy' || node.data("type") === 'premined')
+        return 11;
+      else {
+        var rank = node.data("rank");
+        var level = 0;
+        switch (true) {
+          case rank <= 10:
+            level = 10;
+            break;
+
+          case rank <= 20:
+            level = 9;
+            break;
+
+          case rank <= 30:
+            level = 8;
+            break;
+
+          case rank <= 40:
+            level = 7;
+            break;
+
+          case rank <= 50:
+            level = 6;
+            break;
+
+          case rank <= 60:
+            level = 5;
+            break;
+
+          case rank <= 70:
+            level = 4;
+            break;
+
+          case rank <= 80:
+            level = 3;
+            break;
+
+          case rank <= 90:
+            level = 2;
+            break;
+
+          case rank <= 100:
+            level = 1;
+            break;
+        }
+
+        return level;
+      }
+    },
+    levelWidth: function () { return 1; },
+    padding: 50,
+    spacingFactor: 2,
+  };
+
 
 
   // when both graph export json and style loaded, init cy
@@ -52,7 +117,7 @@ $(function () {
     var erctokensHTML = then[4];
     var ERC20List = scrapeERCTokens(erctokensHTML);
 
-    var totalMarketCap = coinmarketcapData.reduce( (acc, el) => acc + Number(el.market_cap_usd) , 0 );
+    var totalMarketCap = coinmarketcapData.reduce((acc, el) => acc + Number(el.market_cap_usd), 0);
 
 
     var merged = coinmarketcapData.reduce((acc, x) => {
@@ -60,7 +125,7 @@ $(function () {
       var data = cryptocompareData[mapMismatchedSymbols(x.symbol)];
 
       if (!data) {
-        console.log('Missing data', x);
+        console.error('Missing data', x);
         return acc;
       }
 
@@ -70,18 +135,18 @@ $(function () {
       x.proof_type = data ? data.ProofType : "?";
       x.image_url = `./images/${x.symbol.toLowerCase()}.png`;
 
-      if(data) {
-        var mult = Math.log10(x.market_cap_usd  );
-        
-        x.dim = mult * 10 / 2; 
+      if (data) {
+        var mult = Math.log10(x.market_cap_usd);
+
+        x.dim = mult * 10 / 2;
       } else {
         x.dim = 40;
       }
-      
+
       x.type = "crypto";
 
       acc.push({ data: x });
-      console.log("DIM", x.dim);
+      // console.log("DIM", x.dim);
       return acc;
 
     }, []);
@@ -93,7 +158,8 @@ $(function () {
       return acc;
     }, {});
 
-    console.log(merged);
+
+    console.info("Crypto Data", merged);
 
     var elements = {
       nodes: [],
@@ -152,72 +218,51 @@ $(function () {
 
     });
 
-    var layoutOptions ={
-      name: 'concentric',
+    initCy(elements, styles, layoutOptions);
 
+  }
+
+  function highlight(node) {
+
+    var allElements = cy.elements();
+
+    allElements.removeClass('faded');
+    allElements.removeClass('highlighted');
+
+    if (!node.length) {
+      var resetLayout = allElements.makeLayout(layoutOptions);
+
+      resetLayout.run();
+
+      return;
+    }
+
+    var nhood = node.closedNeighborhood();
+    var others = allElements.not(nhood);
+
+    others.addClass('faded');
+    nhood.addClass('highlighted');
+
+    var l = nhood.makeLayout({
+      name: 'concentric',
       fit: true,
       animate: true,
       animationDuration: 500,
       animationEasing: 'linear',
       avoidOverlap: true,
-      concentric: function (node) {
-        if (node.data("type") === 'consensus' || node.data("type") === 'privacy' || node.data("type") === 'premined')
-          return 11;
-        else {
-          var rank = node.data("rank");
-          var level = 0;
-          switch(true){
-            case rank <= 10:
-              level = 10;
-              break;
-
-            case rank <= 20:
-              level = 9;
-              break;
-
-            case rank <= 30:
-              level = 8;
-              break;
-
-            case rank <= 40:
-              level = 7;
-              break;
-            
-            case rank <= 50:
-              level = 6;
-              break;
-
-            case rank <= 60:
-              level = 5;
-              break;
-            
-            case rank <= 70:
-              level = 4;
-              break;
-
-            case rank <= 80:
-              level = 3;
-              break;
-
-            case rank <= 90:
-              level = 2;
-              break;
-            
-            case rank <= 100:
-              level = 1;
-              break;
-          }
-          
-          return level;
+      concentric: function (ele) {
+        if (ele.same(node)) {
+          return 2;
+        } else {
+          return 1;
         }
       },
       levelWidth: function () { return 1; },
       padding: 50,
-      spacingFactor: 2,
-    };
+      spacingFactor: 3,
+    });
 
-    initCy(elements, styles, layoutOptions);
-
+    l.run();
   }
 
   function initCy(elements, styles, layoutOptions) {
@@ -239,81 +284,35 @@ $(function () {
       maxZoom: 4,
     });
 
-    function highlight(node) {
-
-      var allElements = cy.elements();
-
-      allElements.removeClass('faded');
-      allElements.removeClass('highlighted');
-
-      if (!node.length) {
-        var resetLayout = allElements.makeLayout(layoutOptions);
-
-        resetLayout.run();
-
-        return;
-      }
-
-      var nhood = node.closedNeighborhood();
-      var others = allElements.not(nhood);
-
-      others.addClass('faded');
-      nhood.addClass('highlighted');
-
-      var l = nhood.makeLayout({
-        name: 'concentric',
-        fit: true,
-        animate: true,
-        animationDuration: 500,
-        animationEasing: 'linear',
-        avoidOverlap: true,
-        concentric: function (ele) {
-          if (ele.same(node)) {
-            return 2;
-          } else {
-            return 1;
-          }
-        },
-        levelWidth: function () { return 1; },
-        padding: 50,
-        spacingFactor: 3,
-      });
-
-      l.run();
-    }
-
     cy.on('tap', function (evt) {
-      
+
       highlight(evt.target);
       hideNodeInfo();
 
     });
 
-    cy.on('tap', 'node[type="crypto"]',function (evt) {
+    cy.on('tap', 'node[type="crypto"]', function (evt) {
 
-      if( evt.target.length ){
-        showNodeInfo( evt.target );
+      if (evt.target.length) {
+        showNodeInfo(evt.target);
       }
 
     });
 
-    
+
   }
 
-  function showNodeInfo( node ){
-    $('#info').html( infoWidget( node.data() ) ).show();
+  function showNodeInfo(node) {
+    $('#info').html(infoWidget(node.data())).show();
   }
 
-  function hideNodeInfo(){
+  function hideNodeInfo() {
     $('#info').hide();
   }
 
   var infoWidget = Handlebars.compile(
     `<script type="text/javascript" src="https://files.coinmarketcap.com/static/widget/currency.js"></script><div class="coinmarketcap-currency-widget" data-currency="{{id}}" data-base="USD" data-secondary="" data-ticker="true" data-rank="true" data-marketcap="true" data-volume="true" data-stats="USD" data-statsticker="false"></div>`
   );
-
-
-
 
   function getProof(proof) {
 
@@ -353,4 +352,82 @@ $(function () {
     return symbol;
   }
 
-});
+  var infoTemplate = Handlebars.compile([
+    '<p class="ac-name">{{name}}</p>'
+  ].join(''));
+
+
+  var lastSearch = "";
+
+  $('#search').typeahead(
+    {
+      minLength: 2,
+      highlight: true,
+    },
+    {
+      name: 'search-dataset',
+      source: function (query, cb) {
+        function matches(str, q) {
+          str = (str || '').toLowerCase();
+          q = (q || '').toLowerCase();
+
+          return str.match(q);
+        }
+
+        var fields = ['name', 'id', 'proof_type'];
+
+        function anyFieldMatches(n) {
+          for (var i = 0; i < fields.length; i++) {
+            var f = fields[i];
+
+            if (matches(n.data(f), query)) {
+              return true;
+            }
+          }
+
+          return false;
+        }
+
+        function getData(n) {
+          var data = n.data();
+
+          return data;
+        }
+
+        function sortByName(n1, n2) {
+          if (n1.data('name') < n2.data('name')) {
+            return -1;
+          } else if (n1.data('name') > n2.data('name')) {
+            return 1;
+          }
+
+          return 0;
+        }
+
+        var allNodes = cy.nodes();
+        var res = allNodes.stdFilter(anyFieldMatches).sort(sortByName).map(getData);
+
+        cb(res);
+      },
+      templates: {
+        suggestion: infoTemplate
+      }
+    }).on('typeahead:selected', function (e, entry, dataset) {
+      var n = cy.getElementById(entry.id);
+
+      highlight(n);
+      showNodeInfo(n);
+
+    }).on('keydown keypress keyup change', _.debounce(function (e) {
+      var thisSearch = $('#search').val();
+
+      if (thisSearch !== lastSearch) {
+        $('.tt-dropdown-menu').scrollTop(0);
+
+        lastSearch = thisSearch;
+      }
+    }, 50));
+
+
+
+  });
